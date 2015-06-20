2bbb38d986771b1a69b22ac086fb60657f8db036
Paul C Pederson <paul.c.pederson@gmail.com>
2015-06-20 12:32:18 -0700

    working ignore option, ignore all underscore files

--- bin/static-site.js
+++ bin/static-site.js
@@ -25,6 +25,12 @@ var yargs = require('yargs')
       default: defaults.files,
       describe: 'array of file extensions to compile'
     },
+    'i': {
+      alias: 'ignore',
+      type: 'array',
+      default: defaults.ignore,
+      describe: 'array of paths in source folder to ignore'
+    },
     'h': {
       alias: 'helpers',
       type: 'array',
@@ -54,6 +60,10 @@ var yargs = require('yargs')
   .version(require(__dirname + '/../package.json').version + '\n')
   .argv
 
+function green (message) {
+  console.log(color.green(message))
+}
+
 staticSite(yargs, function (err, stats) {
   if (err) {
     console.error(err)
@@ -61,19 +71,19 @@ staticSite(yargs, function (err, stats) {
   }
 
   if (yargs.verbose) {
-    console.log(color.green('Built the following pages:'))
+    green('Built the following pages:')
     var buildPath = path.join(process.cwd(), stats.build)
     stats.pages.forEach(function (page) {
       page = page.replace(buildPath, '')
       console.log('  ' + page)
     })
-    console.log(color.green('Source Folder:'))
+    green('Source Folder:')
     console.log('  ' + stats.source)
-    console.log(color.green('Build Folder:'))
+    green('Build Folder:')
     console.log('  ' + buildPath)
   }
 
   var message = util.format('Built %s files in %sms', stats.pages.length, stats.duration)
-  console.log(color.green(message))
+  green(message)
   process.exit(0)
 })
