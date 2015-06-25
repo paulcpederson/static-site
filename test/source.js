105cc068990a4da61dd536e1ddeec9a5c05ac362
Paul C Pederson <paul.c.pederson@gmail.com>
2015-06-25 07:38:43 -0700

    test for files option

--- test/source.js
+++ test/source.js
@@ -27,3 +27,29 @@ test('custom ignore', function (t) {
     rimraf.sync('test/fixtures/ignore-underscores/build')
   })
 })
+
+var fileOptions = {
+  source: 'test/fixtures/files/source',
+  build: 'test/fixtures/files/build'
+}
+
+test('builds only markdown and html files', function (t) {
+  rimraf.sync('test/fixtures/files/build')
+  t.plan(2)
+  staticSite(fileOptions, function (err, stats) {
+    t.error(err)
+    t.equal(stats.pages.length, 1)
+    rimraf.sync('test/fixtures/files/build')
+  })
+})
+
+test('custom files', function (t) {
+  rimraf.sync('test/fixtures/files/build')
+  t.plan(2)
+  fileOptions.files = ['xml']
+  staticSite(fileOptions, function (err, stats) {
+    t.error(err)
+    t.equal(stats.pages.length, 2)
+    rimraf.sync('test/fixtures/files/build')
+  })
+})
