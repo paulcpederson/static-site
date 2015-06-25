bb2097f308fbf96f99dd02fab1489bb29d575b83
Paul C Pederson <paul.c.pederson@gmail.com>
2015-06-25 07:39:09 -0700

    test that custom files are also rendered correctly with template engine

--- test/template.js
+++ test/template.js
@@ -5,12 +5,13 @@ var fs = require('fs')
 
 var options = {
   source: 'test/fixtures/template/source',
-  build: 'test/fixtures/template/build'
+  build: 'test/fixtures/template/build',
+  files: ['xml']
 }
 
 test('swig layouts and partials', function (t) {
   rimraf.sync('test/fixtures/template/build')
-  t.plan(11)
+  t.plan(12)
   staticSite(options, function (err, stats) {
     t.error(err)
     var file = fs.readFileSync(stats.pages[0], 'utf8')
@@ -21,11 +22,14 @@ test('swig layouts and partials', function (t) {
     t.ok(file.indexOf('thing1 thing2 thing3 ') > -1)
 
     var file2 = fs.readFileSync(stats.pages[1], 'utf8')
-    t.ok(file2.indexOf('title: test title') > -1)
-    t.ok(file2.indexOf('description: test description') > -1)
-    t.ok(file2.indexOf('partial1: test title') > -1)
-    t.ok(file2.indexOf('partial2: test title') > -1)
-    t.ok(file2.indexOf('test1 test2 test3 ') > -1)
+    t.equal(file2, 'xml title')
+
+    var file3 = fs.readFileSync(stats.pages[2], 'utf8')
+    t.ok(file3.indexOf('title: test title') > -1)
+    t.ok(file3.indexOf('description: test description') > -1)
+    t.ok(file3.indexOf('partial1: test title') > -1)
+    t.ok(file3.indexOf('partial2: test title') > -1)
+    t.ok(file3.indexOf('test1 test2 test3 ') > -1)
     rimraf.sync('test/fixtures/template/build')
   })
 })
