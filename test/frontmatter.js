e15211c345cce4839b4bcd11b67ea1525d49317c
Paul C Pederson <paul.c.pederson@gmail.com>
2015-06-26 10:53:11 -0700

    test extra options

--- test/frontmatter.js
+++ test/frontmatter.js
@@ -3,12 +3,11 @@ var rimraf = require('rimraf')
 var staticSite = require('..')
 var fs = require('fs')
 
-var options = {
-  source: 'test/fixtures/frontmatter/source',
-  build: 'test/fixtures/frontmatter/build'
-}
-
 test('frontmatter objects, arrays, and strings', function (t) {
+  var options = {
+    source: 'test/fixtures/frontmatter/source',
+    build: 'test/fixtures/frontmatter/build'
+  }
   rimraf.sync('test/fixtures/frontmatter/build')
   t.plan(4)
   staticSite(options, function (err, stats) {
@@ -18,5 +17,23 @@ test('frontmatter objects, arrays, and strings', function (t) {
     t.ok(file.indexOf('one,two,three') > -1)
     t.ok(file.indexOf('property') > -1)
     rimraf.sync('test/fixtures/frontmatter/build')
+  })
+})
+
+test('extra options are added to frontmatter', function (t) {
+  var options = {
+    source: 'test/fixtures/extra-options/source',
+    build: 'test/fixtures/extra-options/build',
+    ENV: 'production'
+  }
+  rimraf.sync('test/fixtures/extra-options/build')
+  t.plan(3)
+  staticSite(options, function (err, stats) {
+    t.error(err)
+    var file = fs.readFileSync(stats.pages[0], 'utf8')
+    t.equal(file, 'production')
+    var overriddenFile = fs.readFileSync(stats.pages[1], 'utf8')
+    t.equal(overriddenFile, 'development')
+    rimraf.sync('test/fixtures/extra-options/build')
   })
 })
