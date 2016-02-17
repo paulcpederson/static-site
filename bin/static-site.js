2ec501d9544b30fda6ff68f5e728d03ceee0b44c
Paul C Pederson <paul.c.pederson@gmail.com>
2016-02-16 21:16:15 -0800

    remove yargs dependency (#4)

--- bin/static-site.js
+++ bin/static-site.js
@@ -2,48 +2,62 @@
 var staticSite = require('../')
 var path = require('path')
 var util = require('util')
-var yargs = require('yargs')
-  .usage('Usage: $0 [options]')
-  .options({
-    'b': {
-      alias: 'build',
-      type: 'string',
-      describe: 'Path to build folder'
-    },
-    's': {
-      alias: 'source',
-      type: 'string',
-      describe: 'Path to source folder'
-    },
-    'f': {
-      alias: 'files',
-      type: 'array',
-      describe: 'Array of file extensions to compile'
-    },
-    'i': {
-      alias: 'ignore',
-      type: 'array',
-      describe: 'Array of paths in source folder to ignore'
-    },
-    'h': {
-      alias: 'helpers',
-      type: 'array',
-      describe: 'Array of site helpers to run'
-    },
-    't': {
-      alias: 'templateEngine',
-      type: 'string',
-      describe: 'Template engine to use'
-    },
-    'v': {
-      alias: 'verbose',
-      type: 'boolean',
-      describe: 'Enable verbose logging'
-    }
-  })
-  .help('help')
-  .version(require(path.join(__dirname, '..', 'package.json')).version)
-  .argv
+var argvParse = require('argv-parse')
+var pkg = require(path.join(__dirname, '..', 'package.json'))
+
+var argv = argvParse({
+  'build': {
+    alias: 'b',
+    type: 'string'
+  },
+  'source': {
+    alias: 's',
+    type: 'string'
+  },
+  'files': {
+    alias: 'f',
+    type: 'array'
+  },
+  'ignore': {
+    alias: 'i',
+    type: 'array'
+  },
+  'helpers': {
+    alias: 'h',
+    type: 'array'
+  },
+  'templateEngine': {
+    alias: 't',
+    type: 'string'
+  },
+  'verbose': {
+    alias: 'v',
+    type: 'boolean'
+  }
+})
+
+if (argv.help) {
+  var helpText = [
+    'Usage: static-site [options] \n',
+    'Options:',
+    '  -b, --build           Path to build folder',
+    '  -s, --source          Path to source folder',
+    '  -f, --files           Array of file extensions to compile',
+    '  -i, --ignore          Array of paths in source folder to ignore',
+    '  -h, --helpers         Array of site helpers to run',
+    '  -t, --templateEngine  Template engine to use',
+    '  -v, --verbose         Enable verbose logging',
+    '  --version             Show version number',
+    '  --help                Show help text'
+  ].join('\n')
+  console.log(helpText)
+  process.exit(0)
+}
+
+if (argv.version) {
+  console.log(pkg.version)
+  process.exit(0)
+}
 
 function green (message) {
   // use green ANSI terminal code, then set it back to normal
@@ -55,22 +69,17 @@ function indent (message) {
   console.log('  ' + message)
 }
 
-var blacklist = ['_', '$0', 'v', 'verbose', 'help', 'version', 's', 'b', 'f', 't', 'h', 'i']
-var options = {}
+var verbose = argv.verbose
+delete argv.verbose
+delete argv._
 
-Object.keys(yargs).forEach(function (option) {
-  if (yargs[option] !== 'undefined' && blacklist.indexOf(option) === -1) {
-    options[option] = yargs[option]
-  }
-})
-
-staticSite(options, function (err, stats) {
+staticSite(argv, function (err, stats) {
   if (err) {
     console.error(err)
     process.exit(1)
   }
 
-  if (yargs.verbose) {
+  if (verbose) {
     green('Built the following pages:')
     var buildPath = path.join(process.cwd(), stats.build)
     stats.pages.forEach(function (page) {
