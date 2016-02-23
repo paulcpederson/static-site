f95df24f59d8df4aa5845356dc94a95c1803fed2
Paul C Pederson <paul.c.pederson@gmail.com>
2016-02-22 22:50:45 -0800

    each lib task returns a function which can be called with context

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 var lib = require('./lib/')
 var getContext = require('./lib/utils/get-context')
 
-function applyContext (context, library) {
-  return lib[library].bind(null, context)
-}
-
 function staticSite (options, cb) {
-  var context = getContext(options)
-  var bindLib = applyContext.bind(null, context)
-  lib.source.call(context)
-    .then(bindLib('frontmatter'))
-    .then(bindLib('data'))
-    .then(bindLib('helpers'))
-    .then(bindLib('template'))
-    .then(bindLib('build'))
+  var ctx = getContext(options)
+  lib.source(ctx)
+    .then(lib.frontmatter(ctx))
+    .then(lib.data(ctx))
+    .then(lib.helpers(ctx))
+    .then(lib.template(ctx))
+    .then(lib.build(ctx))
     .then(function (pages) {
       var t2 = Date.now()
       var stats = {
         pages: pages,
-        source: context.sourcePath,
-        build: context.build,
-        start: context.start,
+        source: ctx.sourcePath,
+        build: ctx.build,
+        start: ctx.start,
         end: t2,
-        duration: t2 - context.start
+        duration: t2 - ctx.start
       }
       return cb(null, stats)
     })
